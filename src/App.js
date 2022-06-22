d543893a5c24f944d23cac6e44402d68ebd0f06c
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2022-06-22 18:10:09 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
-      <Route exact path="/" render={ (props) => <Login { ...props } /> } />);
+      <Switch>
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
+      </Switch>
+    );
   }
 }
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  state = {
+    despesa: 0,
+    cambio: 'BRL',
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    const { despesa, cambio } = this.state;
+    return (
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">{ despesa }</p>
+        <p data-testid="header-currency-field">{ cambio }</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
